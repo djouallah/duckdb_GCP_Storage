b96e6001296adb8dbea69caf9f50a76ca379acaf
Mimoune <djouallah@users.noreply.github.com>
2022-09-04 19:18:24 +1000

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ except :
 
 ################################################### Download Data from BigQuery#####################################################
 # Retrieve and convert key file content.
-os.environ["SERVICE_ACCOUNT"] = json.loads(st.secrets["key_GCP"], strict=False)
+os.environ["SERVICE_ACCOUNT"] == st.secrets["key_GCP"]
 
 from pyarrow import parquet
 
